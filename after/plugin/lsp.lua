d93f1aa4ed1e8d220055318f528cdbe43c40516a
Kasper <kasper35@live.no>
2023-01-28 02:45:13 +0100

    first commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,31 @@
+local lsp = require('lsp-zero')
+
+vim.opt.signcolumn = 'yes'
+
+lsp.preset('recommended')
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
+lsp.ensure_installed({
+	'tsserver',
+	'eslint',
+	'rust_analyzer'
+})
+
+lsp.setup()
+
+
+vim.diagnostic.config({
+    virtual_text = true
+})
