745c98bc53f942020711b6d47fc3737a9b11971a
qqgameover <kasper35@live.no>
2024-04-02 03:29:41 +0200

    Changed packer to lazy, fugitive to neogit

--- lua/src/packer.lua
+++ lua/src/packer.lua
@@ -1,73 +1,99 @@
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		-- or                            , branch = '0.1.x',
-		requires = {
-            {'nvim-lua/plenary.nvim'},
-            {'BurntSushi/ripgrep'}
-        }
-	}
-	use({
-		'catppuccin/nvim',
-		as = 'catppuccin',
-		config = function()
-            require("catppuccin").setup()
-            vim.cmd('colorscheme catppuccin')
-		end
-	})
-    use{
-        ('folke/noice.nvim'),
-        requires = {
-            'MunifTanjim/nui.nvim',
-            'rcarriga/nvim-notify'
-        }
-    }
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+require('lazy').setup({
+      -- Packer can manage itself
+        {
+            'nvim-treesitter/nvim-treesitter',
+            build = function()
+                pcall(require('nvim-treesitter.install').update {with_sync = true})
+            end
         },
-    }
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
-    use {'nvim-tree/nvim-web-devicons'}
-    use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
-	use {
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v1.x',
-	requires = {
-		-- LSP Support
-		{'neovim/nvim-lspconfig'},             -- Required
-		{'williamboman/mason.nvim'},           -- Optional
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+        'theprimeagen/harpoon',
+        'mbbill/undotree',
+        {
+          "NeogitOrg/neogit",
+          dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},         -- Required
-		{'hrsh7th/cmp-nvim-lsp'},     -- Required
-		{'hrsh7th/cmp-buffer'},       -- Optional
-        {'simrat39/rust-tools.nvim'},
-		{'hrsh7th/cmp-path'},         -- Optional
-		{'saadparwaiz1/cmp_luasnip'}, -- Optional
-		{'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua"
+           },
+          config = true
+        },
+        'christoomey/vim-tmux-navigator',
+        {
+          'nvim-lualine/lualine.nvim',
+          dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
+        },
+        {
+            'nvim-tree/nvim-tree.lua',
+            dependencies = {
+                'nvim-tree/nvim-web-devicons', -- optional, for file icons
+            }
+        },
+        {
+            "windwp/nvim-autopairs",
+            config = function() require("nvim-autopairs").setup {} end
+        },
+        {'nvim-tree/nvim-web-devicons'},
+        {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'},
+        {
+            'nvim-telescope/telescope.nvim', version = '0.1.4',
+            -- or                            , branch = '0.1.x',
+            dependencies = {
+                {'nvim-lua/plenary.nvim'},
+                {'BurntSushi/ripgrep'}
+            }
+        },
+        ({
+            'catppuccin/nvim',
+            as = 'catppuccin',
+            config = function()
+                require("catppuccin").setup()
+                vim.cmd('colorscheme catppuccin')
+            end
+        }),
+        {
+            ('folke/noice.nvim'),
+            dependencies = {
+                'MunifTanjim/nui.nvim',
+                'rcarriga/nvim-notify'
+            }
+        },
+        {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		-- Snippets
-		{'L3MON4D3/LuaSnip'},             -- Required
-		{'rafamadriz/friendly-snippets'}, -- Optional
-	}
-}
-end)
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'simrat39/rust-tools.nvim'},
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
+})
